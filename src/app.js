abd4002a9ce6ad1ec31497c62a228941f1f63590
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-12 15:06:20 +0530

    {} optinal c in path

--- src/app.js
+++ src/app.js
@@ -2,19 +2,9 @@ const express = require("express");
 
 const app = express();
 
-//order matters the most *-all the route matches to this use route and displays **(hahahah)**
-//whether it si get,post,delete method it only displays hahahaha
-app.use("/user", (req, res) => {
-  res.send("hahahaha");
-});
-app.get("/user", (req, res) => {
+//Route Path Pattern Matching {}-optional,
+app.get("/ab{c}d", (req, res) => {
   res.send({ firstname: "kamran", lastname: "Ahmed" });
-});
-app.post("/user", (req, res) => {
-  res.send("Saved data to database");
-});
-app.delete("/user", (req, res) => {
-  res.send("Deleted data sucessfully");
 });
 
 app.listen(7777);
