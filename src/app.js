9f38ed02bab70e00e8250804f8a5ccdf981a1126
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-12 15:20:47 +0530

    when to use "" and regex in url path

--- src/app.js
+++ src/app.js
@@ -4,6 +4,22 @@ const app = express();
 
 // Use Use .*a.* — which means anything, then a, then anything:
 
+
+//------------------------------------------------------------------------------------------------------
+//when to use which regex in path 
+
+// Is your URL fixed or simple?
+//         |
+//        YES → use " " string
+//         |
+//         NO → Does it need flexible/complex matching?
+//                     |
+//                    YES → use / / regex
+
+
+//-------------------------------------------------------------------------------------------------------
+
+
 app.get(/\/.*a.*/, (req, res) => {
   res.send("matched!");
 });
