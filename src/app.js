4afce2e9b04f545289495bb9525471c16aafd045
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-12 14:47:40 +0530

    order matters the most *-all the route matches to this use route and displays **(hahahah)**

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-    app.get("/user",(req, res)=>{
-        res.send({firstname : "kamran", lastname : "Ahmed"});
-    })
-    app.post("/user",(req, res)=>{
-        res.send("Saved data to database");
-    })
-    app.delete("/user",(req, res)=>{
-        res.send("Deleted data sucessfully");
-    })
+//order matters the most *-all the route matches to this use route and displays **(hahahah)**
+//whether it si get,post,delete method it only displays hahahaha
+app.use("/user", (req, res) => {
+  res.send("hahahaha");
+});
+app.get("/user", (req, res) => {
+  res.send({ firstname: "kamran", lastname: "Ahmed" });
+});
+app.post("/user", (req, res) => {
+  res.send("Saved data to database");
+});
+app.delete("/user", (req, res) => {
+  res.send("Deleted data sucessfully");
+});
 
 app.listen(7777);
