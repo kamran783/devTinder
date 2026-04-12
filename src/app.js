70a748484b213540a9b222ca7f3022e3a72f133f
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-12 14:42:14 +0530

    http methods

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use("/login",(req,res)=>{
-    res.send("/login/2")
-})
-app.post("/user",(req, res)=>{
-    res.send({firstname:"kamran", lastname : "ahmed"})
-})
+    app.get("/user",(req, res)=>{
+        res.send({firstname : "kamran", lastname : "Ahmed"});
+    })
+    app.post("/user",(req, res)=>{
+        res.send("Saved data to database");
+    })
+    app.delete("/user",(req, res)=>{
+        res.send("Deleted data sucessfully");
+    })
 
 app.listen(7777);
