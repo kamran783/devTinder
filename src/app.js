08401da67cb1a324e4a7bbf5771bcdd4ad5925df
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-13 15:13:37 +0530

    /handling multiple routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:age", (req, res) => {
-  console.log(req.params);
-  res.send("matched!");
-});
+//handling multiple routes
 
-app.listen(7777, ()=>{
-    console.log("server is listening on port 7777")
+//Always send a response in every route — otherwise browser will wait forever and eventually timeout! ⏳
+
+app.use(
+  "/app",
+  [(req, res, next) => {
+    //res.send("listening server on port777");
+    next()
+  },
+  (req, res, next) => {
+    console.log("2nd route handler");
+    //res.send("2nd route")
+    next()
+  }],[
+  (req,res)=>{
+    res.send("3rd rote")
+  }]
+);
+
+app.listen(7777, () => {
+  console.log("server is listening on port 7777");
 });
