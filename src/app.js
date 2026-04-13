d1b856b4a539d438ef77c11f78d53c0f95b5d1bf
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-13 16:45:28 +0530

    poor way of autthorization of admin

--- src/app.js
+++ src/app.js
@@ -2,25 +2,30 @@ const express = require("express");
 
 const app = express();
 
-//handling multiple routes
 
-//Always send a response in every route — otherwise browser will wait forever and eventually timeout! ⏳
 
-app.use(
-  "/app",
-  [(req, res, next) => {
-    //res.send("listening server on port777");
-    next()
-  },
-  (req, res, next) => {
-    console.log("2nd route handler");
-    //res.send("2nd route")
-    next()
-  }],[
-  (req,res)=>{
-    res.send("3rd rote")
-  }]
-);
+//poor way of autthorization of users
+app.use("/admin/getalldata",(req,res)=>{
+  let token = "xyx";
+  let isAuthorized = token === "xyx";
+  if(isAuthorized){
+    res.send("first one")
+  } else {
+    res.status(401).send("not authorized")
+  }
+})
+app.use("/admin/setalldata",(req,res)=>{
+  let token = "xyx";
+  let isAuthorized = token === "xyx";
+  if(isAuthorized){
+    res.send("second one")
+  } else {
+    res.status(401).send("not authorized")
+  }
+})
+
+
+
 
 app.listen(7777, () => {
   console.log("server is listening on port 7777");
