0b879cb5f2256da0d90f04d506aff8820b9bc5c8
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-15 14:53:36 +0530

    creating instance of user class(mongoose scheme)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user")
 require("dns").setServers(["8.8.8.8", "1.1.1.1"]); 
 
 const app = express();
+app.post("/Signup",async (req,res)=>{
+  //creating instance of user class
+  const user = new User({
+    firstName : "John",
+    lastName : "Doe",
+    email : "John@gmail.com",
+    age : 21,
+    password : "jonny"
 
+  });
+  try {
+
+    await user.save()
+    res.send("user data Added Sucessfully")
+  }catch(err){
+    res.status(400).send("Error saving the user data"+ err.message)
+  }
+})
 connectDB()
   .then(() => {
     console.log("Successfully connected to database ✅");
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,22 @@
-import mongoose from "mongoose"
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-//Schema is a blueprint of how your data will look in the database!
-//without {Schema} you can use (mongoose.Schema({}))
 const userSchema = new Schema({
-    firstName : {
-        type : String,
-    },
-    lastName : {
-        type : String,
-    },
-    Email : {
-        type : String,
-    },
-    age: {
-        type : Number,
-    },
-    password : {
-        type : String,
-    }
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  password: {
+    type: String,
+  },
+});
 
-})
-
-// Model = actual class to interact with DB 🏗️
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
