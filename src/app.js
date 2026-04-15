0a06c5cd8e8717c71e4ff412a6036301c3907094
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-04-15 15:55:16 +0530

    find the user with email and applying crud operations

--- src/app.js
+++ src/app.js
@@ -1,27 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const User = require("./models/user")
-require("dns").setServers(["8.8.8.8", "1.1.1.1"]); 
+const User = require("./models/user");
+require("dns").setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
-app.post("/Signup",async (req,res)=>{
-  //creating instance of user class
-  const user = new User({
-    firstName : "John",
-    lastName : "Doe",
-    email : "John@gmail.com",
-    age : 21,
-    password : "jonny"
 
-  });
+app.use(express.json());
+
+app.post("/Signup", async (req, res) => {
   try {
-
-    await user.save()
-    res.send("user data Added Sucessfully")
-  }catch(err){
-    res.status(400).send("Error saving the user data"+ err.message)
+    let users = await User.find({email : req.body.email})
+    if(users.length === 0){
+      res.status(400).send("No users present")
+    }
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error saving the user data" + err.message);
   }
-})
+});
 connectDB()
   .then(() => {
     console.log("Successfully connected to database ✅");
